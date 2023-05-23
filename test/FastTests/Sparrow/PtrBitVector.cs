ed0f8f6961a5d666f43f18c29c474406cfcd16e2
Maciej Aszyk <maciej@ravendb.net>
2023-05-23 14:15:30 +0200

    tests

--- test/FastTests/Sparrow/PtrBitVector.cs
+++ test/FastTests/Sparrow/PtrBitVector.cs
@@ -1,4 +1,6 @@
-﻿using Sparrow.Binary;
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+using Sparrow.Binary;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -13,7 +15,7 @@ namespace FastTests.Sparrow
         [Fact]
         public void SetIndex()
         {
-            var original = new byte[8] { 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00, 0x00 };
+            var original = new byte[8] {0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00, 0x00};
             fixed (byte* storage = original)
             {
                 var ptr = new PtrBitVector(storage, 64);
@@ -28,7 +30,83 @@ namespace FastTests.Sparrow
                 Assert.Equal(false, ptr[idx - 1]);
                 Assert.Equal(false, ptr[idx]);
                 Assert.Equal(true, ptr[idx + 1]);
-            };
+            }
+
+            ;
+        }
+
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(256)]
+        [InlineData(1024)]
+        [InlineData(1025)]
+        [InlineData(3333)]
+        public void RandomlySetBitsInBitVector(bool forceNonAccelerated, int size)
+        {
+            if (forceNonAccelerated && (Avx2.IsSupported || Sse2.IsSupported) == false)
+                return;
+
+            var buffer = new byte[size];
+            buffer.AsSpan().Clear();
+
+            var bitVector = new BitVector(buffer);
+
+            Assert.Equal(-1, bitVector.IndexOfFirstSetBit());
+
+            var random = new Random(12532423);
+            var boolBuffer = new bool[size];
+
+
+            for (int i = 0; i < size; ++i)
+            {
+                var id = random.Next(0, size);
+                boolBuffer[id] = boolBuffer[id] == false;
+                bitVector[id] = bitVector[id] == false;
+                AssertBitVectorAndBufferCopy(boolBuffer, in bitVector);
+            }
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(256)]
+        [InlineData(1024)]
+        [InlineData(1025)]
+        [InlineData(3333)]
+        public void SetWholeOneByOne(int size)
+        {
+            var bitBuffer = new byte[size];
+            var boolBuffer = new bool[size];
+            bitBuffer.AsSpan().Clear();
+            boolBuffer.AsSpan().Fill(false);
+
+            var bitVector = new BitVector(bitBuffer);
+            Assert.Equal(-1, bitVector.IndexOfFirstSetBit());
+            for (int i = size - 1; i >=0; ++i) 
+            {
+                bitVector[i] = true;
+                boolBuffer[i] = true;
+                var booleanArrayFirstSetBit = IndexOfFirstSetBit();
+                if (Avx2.IsSupported)
+                    Assert.Equal(booleanArrayFirstSetBit, bitVector.IndexOfFirstSetBitAvx2());
+            }
+
+            
+            AssertBitVectorAndBufferCopy(boolBuffer, in bitVector);
+            
+            int IndexOfFirstSetBit()
+            {
+                for (int i = 0; i < size; ++i)
+                    if (boolBuffer[i])
+                        return 1;
+                return -1;
+            }
+        }
+
+        private static void AssertBitVectorAndBufferCopy(Span<bool> buffer, in BitVector bitVector)
+        {
+            for (int i = 0; i < buffer.Length; ++i)
+                Assert.Equal(buffer[i], bitVector[i]);
         }
     }
 }
