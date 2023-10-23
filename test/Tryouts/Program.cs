c2a0c28e0179605e36ea01670e007741565393b0
Maciej Aszyk <maciej@ravendb.net>
2023-10-23 20:09:00 +0200

    ARM Reproduction

--- test/Tryouts/Program.cs
+++ test/Tryouts/Program.cs
@@ -1,12 +1,7 @@
 using System;
-using System.Diagnostics;
-using System.IO;
 using System.Threading.Tasks;
 using Tests.Infrastructure;
-using Raven.Server.Utils;
-using SlowTests.Corax;
-using SlowTests.Sharding.Cluster;
-using Xunit;
+using System.Linq;
 
 namespace Tryouts;
 
@@ -17,47 +12,25 @@ public static class Program
         XunitLogging.RedirectStreams = false;
     }
 
-    public static async Task Main(string[] args)
+    public static  void Main(string[] args)
     {
-        Console.WriteLine(Process.GetCurrentProcess().Id);
-
-        for (int i = 0; i < 1000; i++)
+        var defaultOptions = FastTests.RavenTestBase.Options.ForSearchEngine(RavenSearchEngineMode.Corax);
+        var i = 0;
+        while (true)
         {
-            Console.WriteLine($"Starting to run {i}");
-
             try
             {
-                TryRemoveDatabasesFolder();
-                using (var testOutputHelper = new ConsoleTestOutputHelper())
-                using (var test = new ShardedClusterObserverTests(testOutputHelper))
-                {
-                    DebuggerAttachedTimeout.DisableLongTimespan = true;
-                    await test.ClusterObserverWillSkipCommandIfChangingTheSameDatabaseRecordTwiceInOneIteration();
-                }
+                Console.WriteLine($"Test no {i++}");
+                using var testOutputHelper = new ConsoleTestOutputHelper();
+                using var testClass = new FastTests.Client.Indexing.IndexExtensionFromClient(testOutputHelper);
+                testClass.CanUseMethodFromExtensionsInIndex_WithHashsetReturnType(defaultOptions);
             }
             catch (Exception e)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(e);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-        }
-    }
+                if (e.ToString().Contains("cluster") == false) // sometimes cluster fails due to connections overloads, shouldn't matter tbh
+                    throw;
+                Console.WriteLine("Connection problem");
 
-    private static void TryRemoveDatabasesFolder()
-    {
-        var p = System.AppDomain.CurrentDomain.BaseDirectory;
-        var dbPath = Path.Combine(p, "Databases");
-        if (Directory.Exists(dbPath))
-        {
-            try
-            {
-                Directory.Delete(dbPath, true);
-                Assert.False(Directory.Exists(dbPath), "Directory.Exists(dbPath)");
-            }
-            catch
-            {
-                Console.WriteLine($"Could not remove Databases folder on path '{dbPath}'");
             }
         }
     }
