dfaf84751f3bacdda2fdc92b40f2fc1d0514aaca
Maciej Aszyk <maciej@ravendb.net>
2025-10-01 12:00:05 +0200

    RavenDB-24102 QueryHandler async optimizations.

--- src/Raven.Server/Documents/Indexes/Index.cs
+++ src/Raven.Server/Documents/Indexes/Index.cs
@@ -3339,7 +3339,7 @@ namespace Raven.Server.Documents.Indexes
             DocumentDatabase.QueryMetadataCache.MaybeAddToCache(query.Metadata, Name);
         }
 
-        public virtual async Task<DocumentIdQueryResult> IdQuery(
+        public virtual Task<DocumentIdQueryResult> IdQuery(
             IndexQueryServerSide query,
             QueryOperationContext queryContext,
             DeterminateProgress progress,
@@ -3347,21 +3347,19 @@ namespace Raven.Server.Documents.Indexes
             OperationCancelToken token)
         {
             var result = new DocumentIdQueryResult(progress, onProgress, Definition.ClusterState.LastIndex, token);
-            await QueryInternal(result, query, queryContext, pulseDocsReadingTransaction: false, token: token);
-            return result;
+            return QueryInternal(result, query, queryContext, pulseDocsReadingTransaction: false, token: token);
         }
 
-        public virtual async Task<DocumentQueryResult> Query(
+        public virtual Task<DocumentQueryResult> Query(
             IndexQueryServerSide query,
             QueryOperationContext queryContext,
             OperationCancelToken token)
         {
             var result = new DocumentQueryResult(Definition.ClusterState.LastIndex);
-            await QueryInternal(result, query, queryContext, pulseDocsReadingTransaction: false, token: token);
-            return result;
+            return QueryInternal(result, query, queryContext, pulseDocsReadingTransaction: false, token: token);
         }
 
-        private async Task QueryInternal<TQueryResult>(
+        private async Task<TQueryResult> QueryInternal<TQueryResult>(
             TQueryResult resultToFill,
             IndexQueryServerSide query,
             QueryOperationContext queryContext,
@@ -3621,7 +3619,7 @@ namespace Raven.Server.Documents.Indexes
                             }
                         }
 
-                        return;
+                        return resultToFill;
                     }
                 }
             }
