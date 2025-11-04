6c4296f0beeb091fe5061130d74c2e40378c3587
Maciej Aszyk <maciej@ravendb.net>
2025-11-04 10:30:32 +0100

    Revert "RavenDB-22469 Bug fixes | PR comments"
    This reverts commit 9152a50555e1b0cb7ac5ec9e6eee31f75025f6a2.

--- src/Corax/Utils/GrowableBitArray.cs
+++ src/Corax/Utils/GrowableBitArray.cs
@@ -12,11 +12,13 @@ internal unsafe struct GrowableBitArray : IDisposable
     private readonly long _capacity;
     
     /// <summary>
-    /// Creates a new bit array. It accepts when bits id are between [0, capacity]
+    /// Creates a new growable bit array. It accepts when bits id between [0, capacity]
     /// </summary>
+
     public GrowableBitArray(ByteStringContext allocator, long capacity)
     {
         _capacity = capacity + 1; // ensure it's not zero and handles the last bit inclusively.
+        
         var numberOfUlongsToAllocate = _capacity / 64 + (_capacity % 64 == 0 ? 0 : 1);
         var numberOfBitArrays = (int)Math.Ceiling(numberOfUlongsToAllocate / (double)MaxCapacityPerBitmap);
         _bitArrays = new BitArray[numberOfBitArrays];
@@ -44,12 +46,12 @@ internal unsafe struct GrowableBitArray : IDisposable
 #endif
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public bool Add(long pos)
+    public bool Add(long id)
     {
-        if (pos >= _capacity)
-            throw new ArgumentOutOfRangeException($"Tried to modify the bit at position '{pos}', however the capacity is only {_capacity}");
-        var bitmapIdx = (int)(pos / MaxCapacityPerBitmapInBits);
-        return _bitArrays[(int)bitmapIdx].Add(pos - bitmapIdx * MaxCapacityPerBitmapInBits);
+        if (id > _capacity)
+            throw new ArgumentOutOfRangeException($"Tried to modify bit no {id}, however the capacity is only { _capacity * 64}");
+        var bitmapIdx = (int)(id / MaxCapacityPerBitmapInBits);
+        return _bitArrays[(int)bitmapIdx].Add(id - bitmapIdx * MaxCapacityPerBitmapInBits);
     }
 
     public void Dispose()
@@ -58,7 +60,7 @@ internal unsafe struct GrowableBitArray : IDisposable
             _bitArrays[i].Dispose();
     }
 
-    private struct BitArray : IDisposable
+    private unsafe struct BitArray : IDisposable
     {
         private ulong* _bits;
         private IDisposable _memoryScope;
@@ -91,8 +93,6 @@ internal unsafe struct GrowableBitArray : IDisposable
             IsValid = false;
 #endif
             _memoryScope.Dispose();
-            _bits = null;
-            _memoryScope = null;
         }
     }
 }
--- src/Corax/Utils/GrowableHashSet.cs
+++ src/Corax/Utils/GrowableHashSet.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Corax.Utils;
 
@@ -26,12 +25,12 @@ public sealed class GrowableHashSet<TItem>
     }
 
     public bool HasMultipleHashSets => _hashSetsBucket != null;
-    private const int MaxSizePerCollection = 128_000_000;
+
     public GrowableHashSet(IEqualityComparer<TItem> comparer = null, int? maxSizePerCollection = null)
     {
         _comparer = comparer;
         _hashSetsBucket = null;
-        _maxSizePerCollection = Math.Min(maxSizePerCollection ?? MaxSizePerCollection, MaxSizePerCollection);
+        _maxSizePerCollection = maxSizePerCollection ?? (1 << 27); // 134M items
         CreateNewHashSet();
     }
 
